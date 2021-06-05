a54a5ad331d3ca806787c003d9475895e12c12b5
Maxim <makson202007@gmail.com>
2021-06-05 16:18:15 +0300

    Added finding all .csv files in directories

--- Laboratory2OP/Laboratory2OP.cpp
+++ Laboratory2OP/Laboratory2OP.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <filesystem>
 
-void open_file()
+namespace fs = std::filesystem;
+
 
 int main()
 {
-    std::fstream file;
+    int n, i;
+    n = 1;
+    std::string directory_name = "C:/Users/topkek/source/repos/Laboratory2OP/2Laboratorna/tables";
+    std::string ext = ".csv";
+    std::vector<std::string> files;
+    
+    for (auto& entry : fs::directory_iterator(directory_name))
+    {
+        if (entry.path().extension() == ".csv")
+        {
+            files.push_back(entry.path().generic_string());
+        }
+    }
+
+        
 }
