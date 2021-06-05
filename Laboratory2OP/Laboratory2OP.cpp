f1588fc5e9d0e8f9c7baedc566e1ba511ef81643
Maxim <makson202007@gmail.com>
2021-06-05 16:58:44 +0300

    Added algorithm of reading found files

--- Laboratory2OP/Laboratory2OP.cpp
+++ Laboratory2OP/Laboratory2OP.cpp
@@ -6,11 +6,12 @@
 
 namespace fs = std::filesystem;
 
+std::string* read_files(std::vector<std::string> files, int*);
+
+
 
 int main()
 {
-    int n, i;
-    n = 1;
     std::string directory_name = "C:/Users/topkek/source/repos/Laboratory2OP/2Laboratorna/tables";
     std::string ext = ".csv";
     std::vector<std::string> files;
@@ -23,5 +24,37 @@ int main()
         }
     }
 
+    int n=0;
+    std::string* L = read_files(files, &n);
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << L[i] << std::endl;
+    }
         
+}
+
+std::string* read_files(std::vector<std::string> files, int* n)
+{
+    std::string temp;
+    std::string* DATA = nullptr;
+    for (int i = 0; i < files.size(); i++)
+    {
+        std::fstream outf(files[i]);
+        getline(outf, temp);
+        *n += stoi(temp);
+    }
+    DATA = new std::string[*n];
+    int k = 0 , j = 0;
+    for (int i = 0; i < files.size(); i++)
+    {
+        std::fstream outf(files[i]);
+        getline(outf, temp);
+        k += stoi(temp);
+        while (j < k)
+        {
+            getline(outf, DATA[j]);
+            j++;
+        }
+    }
+    return DATA;
 }
